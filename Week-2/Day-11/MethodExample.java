ceed7033294a47104303eda9c2dd85c269178f32
sangmesh-07 <sangmeshkanni820@gmail.com>
2026-02-21 11:18:25 +0530

    Methods And Parameters Example

---
+++ Week-2/Day-11/MethodExample.java
@@ -0,0 +1,13 @@
+
+public class MethodExample {
+
+    // Method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(10, 20);
+        System.out.println("Sum = " + result);
+    }
+}
