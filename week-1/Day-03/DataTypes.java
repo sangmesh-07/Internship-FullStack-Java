b346bad2b3891d893cccd5ec25c43294f9bbd3d6
sangmesh-07 <sangmeshkanni820@gmail.com>
2026-02-21 10:37:27 +0530

    DataTypes and Variables commit

---
+++ week-1/Day-03/DataTypes.java
@@ -0,0 +1,17 @@
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int age = 21;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isActive = true;
+        String name = "Sangmesh";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + isActive);
+    }
+}
