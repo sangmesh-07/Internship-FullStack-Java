327a58a1337faf2e7ce629a4c5aa913a5265b53e
sangmesh-07 <sangmeshkanni820@gmail.com>
2026-03-10 16:36:51 +0530

    Prepared Statement Example

---
+++ Week-5/Day-26/PreparedStatementExample.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PreparedStatementExample {
+    public static void main(String[] args) {
+
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/studentdb", "root", "password");
+
+            String query = "INSERT INTO students(id, name) VALUES(?, ?)";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, 1);
+            ps.setString(2, "Sangmesh");
+
+            ps.executeUpdate();
+
+            System.out.println("Record inserted successfully");
+
+            con.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
