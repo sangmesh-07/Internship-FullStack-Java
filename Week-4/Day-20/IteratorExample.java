1deb236130f65131e750c419d772b3bfd9015f1c
sangmesh-07 <sangmeshkanni820@gmail.com>
2026-05-04 17:17:27 +0530

    Session Management & Basic Security

--- Week-4/Day-20/IteratorExample.java
+++ Week-4/Day-20/IteratorExample.java
@@ -6,11 +6,10 @@ import java.util.Map;
 public class IteratorExample {
 
     public static void main(String[] args) {
-
         HashMap<Integer, String> students = new HashMap<>();
         students.put(1, "Sangmesh");
-        students.put(2, "Niteesh");
-        students.put(3, "Pankaj");
+        students.put(2, "Rahul");
+        students.put(3, "Anjali");
 
         // Using Iterator
         Iterator<Map.Entry<Integer, String>> iterator = students.entrySet().iterator();
@@ -21,8 +20,3 @@ public class IteratorExample {
         }
     }
 }
-/*output
-ID: 1, Name: Sangmesh
-ID: 2, Name: Niteesh
-ID: 3, Name: Pankaj
-*/  
